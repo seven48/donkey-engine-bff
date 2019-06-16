43a5cb97f315d369c0ae09f03f06c84205678f04
BehindLoader <simonsuprun@gmail.com>
2019-06-16 19:29:23 +0300

    Fix type annotation in settings

--- src/settings.py
+++ src/settings.py
@@ -3,7 +3,7 @@
 import os
 
 
-BFF_SERVER_PORT: int = os.getenv('BFF_SERVER_PORT') or 8000
+BFF_SERVER_PORT: int = int(os.getenv('BFF_SERVER_PORT') or '8000')
 BFF_SERVER_HOST: int = os.getenv('BFF_SERVER_HOST') or 'localhost'
 
 BFF_POSTGRES_URL: str = (
