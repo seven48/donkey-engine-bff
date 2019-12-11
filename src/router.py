d831b9da1ecebed4567a7fc588ccc9d5a4af0cbf
tmcgroul <safonov.a.r@mail.ru>
2019-12-11 19:55:10 +0300

    Bump aiohttp version

--- src/router.py
+++ src/router.py
@@ -8,8 +8,8 @@ from src.users.views.sign_in import view as sign_in_view
 from src.users.views.sign_up import view as sign_up_view
 
 routes = [
-    web.view('/games/', GamesListView),  # type: ignore
-    web.view(r'/games/{id:\d+}', GameView),  # type: ignore
+    web.view('/games/', GamesListView),
+    web.view(r'/games/{id:\d+}', GameView),
     web.route('POST', '/sign_up', sign_up_view),
     web.route('POST', '/sign_in', sign_in_view),
 ]
