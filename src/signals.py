1e35a8c32ff3fd40a2f5cb91b6905597414997c5
sar <safonov.a.r@mail.ru>
2020-01-01 23:32:07 +0300

    Enable autoreload

--- src/signals.py
+++ src/signals.py
@@ -1,10 +1,12 @@
 """Module for aiohttp signals."""
 
+import aioreloader
 from aiohttp.web import Application
 
 
 async def on_startup(app: Application) -> None:
     """Start server operations."""
+    aioreloader.start()
 
 
 async def on_shutdown(app: Application) -> None:
